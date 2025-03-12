fa1552822d766a09fb0b7f219a2f80d46e78346c
Adolfo <adolfododo@hotmail.com>
2025-03-12 07:22:28 -0300

    feat: Add react-scan for performance monitoring

--- steps/src/index.js
+++ steps/src/index.js
@@ -1,8 +1,12 @@
+import { scan } from "react-scan"; 
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-
+scan({
+  enabled: true,
+});
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
